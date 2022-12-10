5aa2b4b2c89747f5d0cf061bbaa72a8de5fa78c2
ArshakyanGrigor <grigor.arshakyan1994@mail.ru>
2022-12-10 14:04:44 +0400

    Updated files

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int charC = 99;
+        int charH = 104;
+        int charA = 97;
+        int charR = 114;
+        System.out.print((char)charC);
+        System.out.print((char)charH);
+        System.out.print((char)charA);
+        System.out.print((char)charR);
+
     }
 }
